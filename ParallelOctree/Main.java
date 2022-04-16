8966906c3ae74c850a6f297ee8f8c00947f2c585
kat0309 <89745552+kat0309@users.noreply.github.com>
2022-04-16 16:49:15 -0400

    Update Main.java

--- ParallelOctree/Main.java
+++ ParallelOctree/Main.java
@@ -10,18 +10,26 @@ import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.Random;
 
 class Main {
-    static BufferedWriter bw;
+    static BufferedWriter bwInsert;
+    static BufferedWriter bwRemove;
+    static BufferedWriter bwInsertRemove;
     static String model = "cube";
     static int minThreads = 1;
     static int maxThreads = 12;
     static int octantLimit = 10;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        bw = new BufferedWriter(new FileWriter("output.txt"));
-        System.out.println("Type\t\t#Threads\t\tRuntime\n");
-        bw.write("Type\t#Threads\tRuntime\n");
+        bwInsert = new BufferedWriter(new FileWriter("V1insertOutput.txt"));
+        bwRemove = new BufferedWriter(new FileWriter("V1removeOutput.txt"));
+        bwInsertRemove = new BufferedWriter(new FileWriter("V1insertRemoveOutput.txt"));
+        
+        System.out.println("Type\t\tTest\t#Threads\t\tRuntime\n");
+        bwInsert.write("Type\tTest\t#Threads\tRuntime\n");
+        bwRemove.write("Type\tTest\t#Threads\tRuntime\n");
+        bwInsertRemove.write("Type\tTest\t#Threads\tRuntime\n");
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-n":
@@ -70,6 +78,15 @@ class Main {
             indexData[i] = Integer.parseInt(indexDataStr[i + 1]);
         }
 
+        // Compute bounds
+        double halfSize = 0;
+        for (Vertex vertex : vertexData) {
+            for (int i = 0; i < 3; i++) {
+                if (Math.abs(vertex.xyz[i]) > halfSize)
+                    halfSize = Math.abs(vertex.xyz[i]);
+            }
+        }
+
         PointerOctree octree = new PointerOctree(octantLimit);
         testOctree(octree, 1, vertexData);
         // testPointerOctree(model, vertexData);
@@ -78,7 +95,6 @@ class Main {
             CoarseGrainOctree cgOctree = new CoarseGrainOctree(octantLimit);
             testOctree(cgOctree, i, vertexData);
 
-            // Working I think
             FineGrainOctree fgOctree = new FineGrainOctree(octantLimit);
             testOctree(fgOctree, i, vertexData);
 
@@ -86,17 +102,21 @@ class Main {
             testOctree(oOctree, i, vertexData);
         }
 
-        bw.close();
+        bwInsert.close();
+        bwRemove.close();
+        bwInsertRemove.close();
     }
 
     static void testOctree(Octree octree, int numThreads, Vertex vertexData[])
             throws InterruptedException, IOException {
-        long start = System.nanoTime();
 
         ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         CompletionService<Void> completionService = new ExecutorCompletionService<>(executor);
         int numVertices = vertexData.length;
 
+        long start = System.nanoTime();
+
+        // Test Insertion
         for (int i = 0; i < numThreads; i++) {
             final int id = i;
             completionService.submit(new Callable<Void>() {
@@ -114,37 +134,102 @@ class Main {
         for (int i = 0; i < numThreads; i++) {
             completionService.take();
         }
-        executor.shutdown();
 
         double execTime = (double) (System.nanoTime() - start) / Math.pow(10, 9);
-        boolean verified = verify(octree, vertexData);
-        printResults(octree.name, numThreads, vertexData.length, execTime, verified);
+        boolean verified = verify(octree, vertexData) == 0;
+        printResults(bwInsert, "Insertion", octree.name, numThreads, vertexData.length, execTime, verified);
+
+        // Test Removal
+        for (int i = 0; i < numThreads; i++) {
+            final int id = i;
+            completionService.submit(new Callable<Void>() {
+                private final int ID = id;
+
+                public Void call() {
+                    for (int i = ID; i < numVertices; i += numThreads) {
+                        octree.remove(vertexData[i]);
+                    }
+                    return null;
+                }
+            });
+        }
+
+        for (int i = 0; i < numThreads; i++) {
+            completionService.take();
+        }
+
+        execTime = (double) (System.nanoTime() - start) / Math.pow(10, 9);
+        verified = verify(octree, vertexData) == numVertices;
+        printResults(bwRemove, "Removal", octree.name, numThreads, vertexData.length, execTime, verified);
+        
+        // Test Insertion/Removal
+        Random rand = new Random();
+        for (int i = 0; i < numThreads; i++) {
+            final int id = i;
+            completionService.submit(new Callable<Void>() {
+                private final int ID = id;
+                int numVerticesInsert = ID;
+                int numVerticesRemove = ID;
+                int action;
+                int[] done = new int[numVertices];
+                public Void call() {
+                    while (numVerticesInsert < numVertices || numVerticesRemove < numVertices)
+                    {
+                        // Randomly get action.
+                        action = rand.nextInt(2);
+                        // If action is set to 0, add the next vertex if it's less than total vertices.
+                        if (action == 0 && numVerticesInsert < numVertices)
+                        {
+                            done[numVerticesInsert] = 1;
+                            octree.insert(vertexData[numVerticesInsert]);
+                            numVerticesInsert += numThreads;
+                        }
+                        // If action is set to 1, remove the vertex from octree if it's already been added in.
+                        else if (action == 1 && done[numVerticesRemove] == 1)
+                        {
+                            octree.remove(vertexData[numVerticesRemove]);
+                            numVerticesRemove += numThreads;
+                        }
+                    }
+                    return null;
+                }
+            });
+        }
+        
+        executor.shutdown();
+        
+        for (int i = 0; i < numThreads; i++) {
+            completionService.take();
+        }
+        
+        execTime = (double) (System.nanoTime() - start) / Math.pow(10, 9);
+        verified = verify(octree, vertexData) == numVertices;
+        printResults(bwInsertRemove, "Insertion/Removal", octree.name, numThreads, vertexData.length, execTime, verified);
     }
 
     // TODO: Other error checking
-    static boolean verify(Octree octree, Vertex vertexData[]) {
-        boolean verified = true;
+    static int verify(Octree octree, Vertex vertexData[]) {
         int count = 0;
         for (var v : vertexData) {
             if (!octree.contains(v)) {
-                // System.err.println("Error: Vertex " + v + " not found");
-                verified = false;
                 count++;
             }
         }
         System.out.println("Missing: " + count);
-        return verified;
+        return count;
     }
 
-    static void printResults(String name, int numThreads, int numVertices, double runtime, boolean verified)
+    static void printResults(BufferedWriter file, String testType, String name, int numThreads, int numVertices,
+            double runtime, boolean verified)
             throws IOException {
         String results = name + "\t" +
-                // model + "\t" +
-                numThreads + "\t" +
-                // numVertices + "\t" +
-                // verified + "\t" +
-                runtime + "\n";
+                            testType + "\t" +
+                            // model + "\n" +
+                            numThreads + "\t" +
+                            // numVertices + "\n" +
+                            // verified + "\n" +
+                            runtime + "\n";
         System.out.print(results);
-        bw.write(results);
+        file.write(results);
     }
 }
